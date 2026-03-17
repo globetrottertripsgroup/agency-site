a21f3c500c1a3fc8440ac11475b8a231f1732cf9
globetrottertripsgroup <globetrottertripsgroup@gmail.com>
2026-03-17 14:48:08 -0500

    Delete quiz.js

--- quiz.js
+++
@@ -1,7 +0,0 @@
-
-document.getElementById("quizForm").addEventListener("submit", function(e){
-e.preventDefault();
-const trip=e.target.tripType.value;
-localStorage.setItem("tripType",trip);
-window.location.href="results.html";
-});
